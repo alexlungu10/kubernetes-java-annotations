795a11a3644fd02cdd8d1e4735416916292ed47f
alexlungu10 <alexlungu10@users.noreply.github.com>
2019-01-19 11:48:43 +0200

    Kubernetes Deployment with an exposed Service on port 80

--- src/main/java/com/alexandru/lungu/kubernetes/annotations/example/kubernetesjavaannotations/KubeController.java
+++ src/main/java/com/alexandru/lungu/kubernetes/annotations/example/kubernetesjavaannotations/KubeController.java
@@ -1,15 +1,16 @@
 package com.alexandru.lungu.kubernetes.annotations.example.kubernetesjavaannotations;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class KubeController {
+    @Value("${appName}")
+    String appName;
     @GetMapping("/")
     public String getRoot() {
-       
-        String DEFAULT_MSG = "The microservice 1 is up and running";
-        return DEFAULT_MSG;
+        return appName;
     }
 
 }
